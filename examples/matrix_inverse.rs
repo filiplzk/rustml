41f1f07da2244fc1ae23aaddfdd07558548e2646
filip <filip.liz77@gmail.com>
2025-07-30 12:43:16 +0200

    Revert "updated matrix_inverse (10-50x times slower than with new engine version, see tensor-grad branch)"
    This reverts commit 96f9b1f51ff1b130ea71c8c71565b7fccd85132c.

--- examples/matrix_inverse.rs
+++ examples/matrix_inverse.rs
@@ -1,52 +1,44 @@
-use std::time::Instant;
-use rustml::*;
+// use rustml::*;
 
+fn main() { }
+//     let mut rng = rand::rng();
 
-fn main() {
-    let start: Instant = Instant::now();
-    let mut rng = rand::rng();
+//     // parameters
+//     let batch_size = 2;
+//     let range = 1.0_f32;
+//     let n = 2;
+//     let iters = 10000;
+//     let lr = 1e-2_f32;
+//     let print_step = 10;
+//     // ----------
 
-    // parameters
-    let batch_size = 1;
-    let range: f32 = 1.0;
-    let n = 32;
-    let iters = 5000;
-    let lr: f32 = 1e0;
-    let print_step = 1000;
-    // ----------
+//     let m1 = Tensor::new_uniform(&mut rng, [batch_size, n, n], -range..range);
 
-    let m1 = Tensor::new_uniform(&mut rng, [batch_size, n, n], -range..range);
+//     let mut tgt = Tensor::fill([batch_size, n, n], 0.0);
+//     for b in 0..batch_size {
+//         for i in 0..n {
+//             tgt[[b, i, i]] = Scalar::new(1.0);
+//         }
+//     }
 
-    let mut tgt = Tensor::fill([batch_size, n, n], 0.0);
-    for b in 0..batch_size {
-        for i in 0..n {
-            tgt[[b, i, i]] = Scalar::new(1.0);
-        }
-    }
+//     let mut m2 = Tensor::fill([batch_size, n, n], 1.0).with_grad();
 
-    let mut m2 = Tensor::fill([batch_size, n, n], 1.0).with_grad();
-
-    for i in 0..iters {
-        let prod = m1.mat_mul(&m2);
-        let loss = functional::mse(&prod, &tgt);
-
-        let topo = loss.backward();        
-        for scalar in m2.flat_mut() {
-            scalar.set(scalar.val() - scalar.grad() * lr);
-        }
-        loss.backward_reset(topo);
+//     for i in 0..iters {
+//         let prod = m1.mat_mul(&m2);
+//         // let loss = functional::mse(&prod, &tgt);
+//         let loss = Scalar::new(1.0).with_grad();
+//         loss.backward();
         
-        if i % print_step == 0 {
-            println!("iteration {}: loss={}", i, loss.val());   
-        }
-    }
-    
-    println!("{}", &m1.mat_mul(&m2));
+//         for scalar in m2.flat_mut() {
+//             scalar.set(scalar.val() - scalar.grad() * lr);
+//         }
 
-    let elapsed_time_s = start.elapsed().as_millis() as f32 / 1000.0;
-    let op_count = batch_size * iters * n * n * n;
-    let op_count_per_sec = op_count as f32 / elapsed_time_s;
-    println!("Execution time: {:.2}s", elapsed_time_s);
-    println!("Estimated number of arithmetic operations: {:.2e}", op_count);
-    println!("Estimated FLOPS: {:.2e}", op_count_per_sec);
-}
+//         loss.backward_reset();
+        
+//         if i % print_step == 0 {
+//             println!("iteration {}: loss={}", i, loss.val());   
+//         }
+//     }
+    
+//     println!("{}", &m1.mat_mul(&m2));
+// }
